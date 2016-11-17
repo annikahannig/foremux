d2ac326a85489086cccae9803c3c33db771840fb
Matthias Hannig <matthias@hannig.cc>
2016-11-17 21:14:38 +0100

    added window splitting and reordering

--- foremux/tmux.py
+++ foremux/tmux.py
@@ -1,4 +1,3 @@
-
 """
 Tmux wrapper
 """
@@ -7,17 +6,62 @@ import subprocess
 
 def split_v():
     """Split tmux screen vertically"""
+    subprocess.call(['tmux', 'split-window', '-v'])
+
+
+def split_h():
+    """Split tmux screen horizontally"""
     subprocess.call(['tmux', 'split-window', '-h'])
 
 
-
-class Layout(object):
-    """Define a tmux layout"""
-
-    def apply(self):
-        """Send tmux commands"""
-        pass
+def select_pane(pane_id):
+    """Go to pane in split layout"""
+    subprocess.call(['tmux', 'select-pane', '-t', pane_id])
 
 
+def send_keys(keys, pane_id=0):
+    """Send command sequence to pane"""
+    subprocess.call(['tmux', 'send-keys', '-t', str(pane_id)] + keys)
 
+
+def run_command(command, pane_id=0):
+    """Execute command in pane"""
+    send_keys([command, 'Enter'], pane_id)
+
+
+def next_layout():
+    """Reorder panes with next layout"""
+    subprocess.call(['tmux', 'next-layout'])
+
+
+def previous_layout():
+    """Reorder panes with previous layout"""
+    subprocess.call(['tmux', 'previous-layout'])
+
+
+def select_layout(preset):
+    """
+    Select a specific layout. Default layouts
+    include:
+        main-vertical
+        main-horizontal
+        even-vertical
+        even-horizontal
+        tiled
+    """
+    subprocess.call(['tmux', 'select-layout', preset])
+
+
+def tiled_layout():
+    """Shortcut for tiled window"""
+    select_layout('tiled')
+
+
+def split_n(panes):
+    """Split and reorder panes"""
+    for i in range(1, panes):
+        split_h()
+
+    # Reorder panes
+    next_layout()
 
